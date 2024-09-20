26782b687424ac0a6f8847e72b12f16101e7e9dd
Fernando Castro <98429775+devfercastro@users.noreply.github.com>
2024-09-20 21:08:39 +0000

    feat: base home template - displays a list of links that renders the countryInfo component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import Image from "next/image";
+import { getAvaillableCountries } from "./services/countryService";
+import Link from "next/link";
 
 export default async function Home() {
+	const availableCountries = await getAvaillableCountries();
+	console.log(availableCountries);
 	return (
-		<main className="flex items-center justify-center h-screen w-screen">
-			<h1 className="text-4xl">Country App</h1>
+		<main className="flex flex-col items-center justify-center w-screen py-8">
+			<h1 className="text-4xl mb-5">Country App</h1>
+			<div className="grid grid-cols-5 gap-x-8 gap-y-5">
+				{availableCountries?.map((country) => (
+					<Link
+						href={`/country/${country.countryCode}${country.name}`}
+						key={country.countryCode}
+						className="bg-slate-900 rounded-lg p-4 text-center hover:bg-slate-50 hover:text-black"
+					>
+						{country.name}
+					</Link>
+				))}
+			</div>
 		</main>
 	);
 }
