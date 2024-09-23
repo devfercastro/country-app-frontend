6216a22c419c1e7690b88bf0a5643cd7c47b2260
Fernando Castro <98429775+devfercastro@users.noreply.github.com>
2024-09-23 22:04:45 +0000

    feat(popChart): the legend now follows the mouse - make it client component - save mouse position in state - save hover status is state - display legend based on mouse position only when chart is hover

--- app/components/population-chart.tsx
+++ app/components/population-chart.tsx
@@ -1,35 +1,78 @@
+"use client";
+
+import { useState } from "react";
 import type { populationItem } from "../common/definitions";
 import { formatNumber } from "../common/utils";
+
+type MousePosition = {
+	x: number;
+	y: number;
+};
 
 export default function PopulationChart({ data }: { data: populationItem[] }) {
 	const max = Math.max(...data.map((item) => item.value));
 	const min = Math.min(...data.map((item) => item.value));
 
+	const [mousePosition, setMousePosition] = useState<MousePosition>({
+		x: 0,
+		y: 0,
+	});
+	const [isHovered, setIsHovered] = useState<boolean>(false);
+
+	const handleMouseEnter = () => setIsHovered(true);
+	const handleMouseLeave = () => setIsHovered(false);
+
+	const handleMouseMove = (
+		event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+	) => {
+		// Get chart position relative to viewport
+		const chartRect = event.currentTarget.getBoundingClientRect();
+		const { clientX, clientY } = event;
+
+		// Calculate mouse position relative to the chart
+		setMousePosition({
+			x: clientX - chartRect.left,
+			y: clientY - chartRect.top,
+		});
+	};
+
 	return (
 		<div className="flex flex-col w-min h-min">
-			<div className="flex flex-row w-min h-min border-l-2 border-b-2 p-2">
-				{data?.reverse().map((item) => (
+			<div className="flex flex-row w-min h-min border-l-2 border-b-2 p-2 relative">
+				{[...data].reverse().map((item) => (
 					<div
 						key={item.year}
 						id={`${item.year}`}
 						className="relative h-[350px] w-4 group"
+						onMouseEnter={handleMouseEnter}
+						onMouseLeave={handleMouseLeave}
+						onMouseMove={handleMouseMove}
 					>
+						{isHovered && (
+							<div
+								className="absolute left-8 bottom-0 hidden group-hover:flex flex-col bg-white p-2 text-xs text-black rounded-lg shadow-lg h-min z-50"
+								style={{
+									left: mousePosition.x + 15, // Offset to the right of the mouse
+									top: mousePosition.y + 20, // Offset bellow the mouse
+								}}
+							>
+								<span>Year: {item.year}</span>
+								<span>Population: {formatNumber(String(item.value))}</span>
+							</div>
+						)}
+
 						<p
-							className="font-extrabold absolute left-0"
+							className="font-extrabold text-xl absolute left-0 cursor-default text-center"
 							style={{ bottom: `${((item.value - min) / (max - min)) * 90}%` }}
 						>
 							.
 						</p>
-						<div className="absolute left-8 bottom-0 hidden group-hover:flex flex-col bg-white p-2 text-xs text-black rounded-lg shadow-lg">
-							<span>Year: {item.year}</span>
-							<span>Population: {formatNumber(String(item.value))}</span>
-						</div>
 					</div>
 				))}
 			</div>
 			<div className="flex flex-row justify-between w-full">
-				<p>{data[0].year}</p>
 				<p>{data.slice(-1)[0].year}</p>
+				<p>{data[0].year}</p>
 			</div>
 		</div>
 	);
