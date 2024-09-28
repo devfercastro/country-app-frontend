4e35e5f2186f5248afeddc8a8e8198037c4ef045
Fernando Castro <98429775+devfercastro@users.noreply.github.com>
2024-09-28 00:34:35 +0000

    refactor(CountryList): container of searched countries takes full parent width

--- app/components/country-list.tsx
+++ app/components/country-list.tsx
@@ -70,7 +70,7 @@ export default function CountryList({
 					</div>
 				))}
 			{query !== "" && (
-				<div className="grid grid-cols-4 gap-4">
+				<div className="grid grid-cols-4 gap-4 w-full">
 					{searchedCountries.map((country) => (
 						<CountryLink
 							key={country.countryCode}
