50c519a00a98d84dfb95aac87c34ce1e1a18637b
Fernando Castro <devfercastro@gmail.com>
2024-10-01 15:30:29 -0300

    fix(services): add timeout and request retrying to manage render sleep. - add axios-retry dep - setup an axios instance and passed to axiosRetry

--- app/services/countryService.ts
+++ app/services/countryService.ts
@@ -1,11 +1,28 @@
 import axios from "axios";
+import axiosRetry from "axios-retry";
+
 import type { APICountriesList, APICountryInfo } from "../common/definitions";
 
 const api = process.env.API_ENDPOINT;
 
+const axiosInstance = axios.create({
+	url: api,
+	timeout: 10000, // In case Render sleep the API
+})
+
+axiosRetry(axiosInstance,
+	{
+		retries: 3,
+		retryDelay: (retryCount) => {
+			console.log(`retry attemp: ${retryCount}`);
+			return retryCount * 2000; // Wait 2'' before retrying
+		}
+	}
+)
+
 export async function getAvaillableCountries() {
 	try {
-		const response = await axios.get<APICountriesList>(`${api}/`);
+		const response = await axiosInstance.get<APICountriesList>(`${api}/`);
 		return response.data;
 	} catch (error) {
 		console.error(error);
@@ -20,7 +37,7 @@ export async function getCountryInfo(name: string, iso2: string) {
 		flag: "",
 	};
 	try {
-		const response = await axios.post<APICountryInfo>(`${api}/country`, {
+		const response = await axiosInstance.post<APICountryInfo>(`${api}/country`, {
 			country: name,
 			iso2,
 		});
